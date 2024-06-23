83f325081f736ff5c78e163c7eb4d1f885e9c88d
ENate <nathaniel@aims.ac.za>
2024-06-23 13:07:56 +0200

    Implemented order-service integration test

--- order-service/src/test/java/com/enate/microservices/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/enate/microservices/orderservice/OrderServiceApplicationTests.java
@@ -1,5 +1,60 @@
 package com.enate.microservices.orderservice;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+
+import io.restassured.RestAssured;
+// Static imports
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.Matchers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OrderServiceApplicationTests {
+
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.3.0");
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @Test
+    void shouldSubmitOrder() {
+        String submitOrderJson = """
+                {
+                    "skuCode": "iphone_15",
+                    "price": 1000,
+                    "quantity": 1
+                }
+                """;
+
+        var orderRequestBodyStr = RestAssured.given()
+                .contentType("application/json")
+                .body(submitOrderJson)
+                .when()
+                .post("/api/order")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body().asString();
+
+        // Test the OrderController placeOrder api
+        assertThat(orderRequestBodyStr, Matchers.is("Order Placed Successfully!"));
+    }
+
+
 
 }
